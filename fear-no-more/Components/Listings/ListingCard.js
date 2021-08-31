43f3f665d249ef576a7a94869df8ac0e58bb5b68
Erin O'Connor <oconnor.wa@gmail.com>
2021-08-31 14:58:20 -0700

    added material-ui grid to card list, renamed listings/listings path to listings/list

--- fear-no-more/Components/Listings/ListingCard.js
+++ fear-no-more/Components/Listings/ListingCard.js
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import AccessibilityNewTwoToneIcon from '@material-ui/icons/AccessibilityNewTwoTone';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
@@ -11,44 +15,62 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    flexGrow: 1
+  },
+  card: {
     maxWidth: 345,
   },
   media: {
-    height: 0,
     paddingTop: '56.25%', // 16:9
     backgroundColor: '#6f180e'
   },
- expand: {
-   marginLeft: 'auto',
- },
+  expand: {
+    marginLeft: 'auto',
+  },
+  cardContent: {
+    height: '150px'
+  },
 }));
 
-export default function RecipeReviewCard(props) {
+export default function ListingCard(props) {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
-      <CardMedia
-        className={classes.media}
-        image={props.imageURL || "/logo-ii.png"}
-        title="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          I left my job, decided to do a coding bootcamp that costs $20,000 and I have a wife and two children.
-          They said I could get a job making a lot of money but need something to tide me over til then...
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <CommentTwoToneIcon />
-        </IconButton>
-        <IconButton
-          className={classes.expand}
-          aria-label="share">
-          <ScreenShareTwoToneIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
+    <Grid item xs={12} sm={6}>
+      <Card className={classes.card}>
+        <CardMedia
+          className={classes.media}
+          image={props.imageURL || "/logo-ii.png"}
+          title="Image of something..." />
+        <CardContent className={classes.cardContent}>
+          <Typography variant='h6' color='textPrimary'>
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.words}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <Button
+            variant="contained"
+            color="secondary"
+            className={classes.button}
+            startIcon={<AccessibilityNewTwoToneIcon />}>
+            Reach Out
+          </Button>
+          <IconButton
+            color="secondary"
+            aria-label="comment">
+            <CommentTwoToneIcon />
+          </IconButton>
+          <IconButton
+            color="secondary"
+            className={classes.expand}
+            aria-label="share">
+            <ScreenShareTwoToneIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </Grid>
   );
 }
