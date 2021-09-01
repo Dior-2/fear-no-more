87d7d14755b8e1657fe7d9c8a043027d8ca7b45d
ehdtlaos <eee910413@gmail.com>
2021-09-01 12:25:35 -0700

    working on nav bar in a different way so I can build mobile version as well

--- fear-no-more/Components/Nav/Header.js
+++ fear-no-more/Components/Nav/Header.js
@@ -1,12 +1,52 @@
+import React, { useState } from 'react';
+
 import Link from 'next/link'
 import headerStyles from '../../styles/Header.module.css'
+import Image from 'next/image'
+import Logo from '../../styles/Logo.png'
+
+import { ReactElement } from 'react'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export default function Header() {
+
+  const [isActive, setActive] = useState(true);
+
+  const menuClick = () => {
+    setActive(!isActive);
+  }
+
   return (
     <div>
-      <div className={headerStyles.background}>
+      <nav className={headerStyles.navbar}>
+        <div className={headerStyles.navbar_logo}>
+          <Image src={Logo} className={headerStyles.navbar_logo_detail} alt='Logo' />
+        </div>
+        <ul className={isActive ? headerStyles.navbar_menu : headerStyles.navbar_menu_gone}>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>Request</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a>Offer</a>
+            </Link>
+          </li>
+        </ul>
+        <a href="#" className={headerStyles.navbar_toggleBtn} onClick={menuClick} >
+          <FontAwesomeIcon icon={faBars} />
+        </a>
+      </nav>
+      {/* <div className={headerStyles.background}>
       <div className={headerStyles.background_logo}>
-
 
       <div className={headerStyles.navBlock}>
         <nav className={headerStyles.nav}>
@@ -42,7 +82,7 @@ export default function Header() {
       </div>
       </div>
       </div>
-
+ */}
 
     </div>
   )
