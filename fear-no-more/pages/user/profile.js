d828aff47084c903a35c9976b6ed164f5be6b949
nick <nickvaline@gmail.com>
2021-09-01 15:08:22 -0700

    ..

--- fear-no-more/pages/user/profile.js
+++ fear-no-more/pages/user/profile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ProfileListItem from '../../Components/User/ProfileListItem';
+import ProfileItem from '../../Components/User/ProfileItem';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import { Container, List } from '@material-ui/core';
 import {
@@ -32,7 +32,7 @@ const Profile = ({ /*user*/ }) => {
   const [user, setUser] = useState({});
 
   useEffect(() => {
-    getUserData('bluigi12@fda.gov');
+    getUserData('bear@bear.com');
   }, []);
 
   const getUserData = async (email) => {
@@ -47,7 +47,7 @@ const Profile = ({ /*user*/ }) => {
     const data = {...user, ogemail: user.email, [name]: userInfo};
 
     await axios.put(`http://18.222.198.9/api/profile`, data)
-      .then(() => getUserData('bluigi12@fda.gov'))
+      .then(() => getUserData('bearb@bear.com'))
       .catch(err => console.log(err))
   };
 
@@ -66,12 +66,12 @@ const Profile = ({ /*user*/ }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <ProfileListItem label="Username"     text={user.username}     name="username" submit={ updateProfile }/>
-            <ProfileListItem label="Password"     text="******"            name="password" submit={ updateProfile }/>
-            <ProfileListItem label="First Name"   text={user.firstname}    name="firstname" submit={ updateProfile }/>
-            <ProfileListItem label="Last Name"    text={user.lastname}     name="lastname" submit={ updateProfile }/>
-            <ProfileListItem label="Role"         text={user.role}         name="role" submit={ updateProfile }/>
-            <ProfileListItem label="Organization" text={user.organization} name="organization" submit={ updateProfile }/>
+            <ProfileItem label="Username"     text={user.username}     name="username" submit={ updateProfile }/>
+            <ProfileItem label="Password"     text="******"            name="password" submit={ updateProfile }/>
+            <ProfileItem label="First Name"   text={user.firstname}    name="firstname" submit={ updateProfile }/>
+            <ProfileItem label="Last Name"    text={user.lastname}     name="lastname" submit={ updateProfile }/>
+            <ProfileItem label="Role"         text={user.role}         name="role" submit={ updateProfile }/>
+            <ProfileItem label="Organization" text={user.organization} name="organization" submit={ updateProfile }/>
           </TableBody>
           <TableHead>
             <TableRow style={{ backgroundColor: 'grey', height: '2.5rem'}}>
@@ -83,10 +83,10 @@ const Profile = ({ /*user*/ }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <ProfileListItem label="Home Phone"     text={user.homephone}        name="homephone" submit={ updateProfile }/>
-            <ProfileListItem label="Cell Phone"     text={user.mobile}           name="mobile" submit={ updateProfile }/>
-            <ProfileListItem label="Email"          text={user.email}            name="email" submit={ updateProfile }/>
-            <ProfileListItem label="Contact Method" text={user.preferredcontact} name="preferredcontact" submit={ updateProfile }/>
+            <ProfileItem label="Home Phone"     text={user.homephone}        name="homephone" submit={ updateProfile }/>
+            <ProfileItem label="Cell Phone"     text={user.mobile}           name="mobile" submit={ updateProfile }/>
+            <ProfileItem label="Email"          text={user.email}            name="email" submit={ updateProfile }/>
+            <ProfileItem label="Contact Method" text={user.preferredcontact} name="preferredcontact" submit={ updateProfile }/>
           </TableBody>
           <TableHead>
           <TableRow style={{ backgroundColor: 'grey', height: '2.5rem'}}>
@@ -98,11 +98,11 @@ const Profile = ({ /*user*/ }) => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <ProfileListItem label="City"      text={user.city}     name="city" submit={ updateProfile }/>
-            <ProfileListItem label="State"     text={user.state}    name="state" submit={ updateProfile }/>
-            <ProfileListItem label="Zip"       text={user.zip}      name="zip" submit={ updateProfile }/>
-            <ProfileListItem label="Address 1" text={user.address1} name="address1" submit={ updateProfile }/>
-            <ProfileListItem label="Address 2" text={user.address2} name="address2" submit={ updateProfile }/>
+            <ProfileItem label="City"      text={user.city}     name="city" submit={ updateProfile }/>
+            <ProfileItem label="State"     text={user.state}    name="state" submit={ updateProfile }/>
+            <ProfileItem label="Zip"       text={user.zip}      name="zip" submit={ updateProfile }/>
+            <ProfileItem label="Address 1" text={user.address1} name="address1" submit={ updateProfile }/>
+            <ProfileItem label="Address 2" text={user.address2} name="address2" submit={ updateProfile }/>
           </TableBody>
         </Table>
       </TableContainer>
