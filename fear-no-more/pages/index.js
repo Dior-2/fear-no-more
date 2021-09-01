76ec715915015b0bfb76dc31482394f20a77aeac
Joseph Peck <joe.r.peck@gmail.com>
2021-09-01 15:23:15 -0700

    Updating main

--- fear-no-more/pages/index.js
+++ fear-no-more/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Layout, { siteTitle } from '../components/layout';
+import Layout, { siteTitle } from '../Components/layout';
 import Testimony from '../Components/Home/Testimonials'
 import Mission from '../Components/Home/Mission'
 import Help from '../Components/Home/Help'
